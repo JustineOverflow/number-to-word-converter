f8e17cb20534420650218b7710106d66a1b5c13e
Justine Fabre <justinefabre84@gmail.com>
2020-09-15 11:13:37 +0100

    add converter algorithm

--- backend/src/converter/converter.controller.spec.ts
+++ backend/src/converter/converter.controller.spec.ts
@@ -2,7 +2,16 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { ConverterController } from './converter.controller';
 
 describe('converter', () => {
+  let converterController: ConverterController;
+
+  beforeEach(async () => {
+    const converter: TestingModule = await Test.createTestingModule({
+      controllers: [ConverterController],
+    }).compile();
+    converterController = converter.get<ConverterController>(ConverterController);
+  });
+
   it('should convert a number into list of words',  () => {
-    expect(ConverterController.convertNumberToString('23')).toBe('[ad, ae, af, bd, be, bf, cd, ce, cf]')
+    expect(converterController.convertNumberToString('23')).toBe("ad,bd,cd,ae,be,ce,af,bf,cf")
   });
 });
--- backend/src/converter/converter.controller.ts
+++ backend/src/converter/converter.controller.ts
@@ -1,6 +1,32 @@
 import { Controller, Get } from '@nestjs/common';
 
+const keyboardLetters = {
+  2: ['a', 'b', 'c'],
+  3: ['d', 'e', 'f'],
+  4: ['g', 'h', 'i'],
+  5: ['j', 'k', 'l'],
+  6: ['m', 'n', 'o'],
+  7: ['p', 'q', 'r', 's'],
+  8: ['t', 'u', 'v'],
+  9: ['w', 'x', 'y', 'z'],
+};
+
 @Controller('converter')
 export class ConverterController {
+
+  @Get()
+  convertNumberToString(number: string) {
+    let combinations = [''];
+    for (const digits of number) {
+      const combinationPerInteraction = [];
+      for (const letter of keyboardLetters[digits]) {
+        for (const combination of combinations) {
+            combinationPerInteraction.push(combination + letter);
+        }
+      }
+      combinations = combinationPerInteraction;
+    }
+    return combinations.toString();
+  }
 }
 
