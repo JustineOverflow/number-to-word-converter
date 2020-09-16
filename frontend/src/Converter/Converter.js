65f6194b938f6ef14d909c7fd6922fbb25ff15bf
Justine Fabre <justinefabre84@gmail.com>
2020-09-16 11:21:55 +0100

    improved ui

--- frontend/src/Converter/Converter.js
+++ frontend/src/Converter/Converter.js
@@ -11,18 +11,24 @@ class Converter extends React.Component {
         }
         this.updateNumber = this.updateNumber.bind(this)
         this.submitNumber = this.submitNumber.bind(this)
+        this.cancelNumber = this.cancelNumber.bind(this)
     }
 
     updateNumber(event) {
+        const newNumber = this.state.number + event;
         this.setState({
-            number: event.target.value,
+            number: newNumber,
         });
-        console.log(this.state.number)
+    }
+
+    cancelNumber() {
+        this.setState({
+            number: '',
+        });
     }
 
     async submitNumber(event) {
         try {
-            debugger
             let response = await fetch(`http://127.0.0.1:3001/converter?number=${this.state.number}`, {
                 method: 'GET',
                 headers: {
@@ -32,11 +38,13 @@ class Converter extends React.Component {
             if (response.status === 400) {
                 this.setState({
                     error: (await response.json()).reason,
+                    number: '',
                     wordList: []
                 })
             } else {
                 this.setState({
                     error: '',
+                    number: '',
                     wordList: (await response.json()).combinations
                 })
             }
@@ -49,64 +57,65 @@ class Converter extends React.Component {
     render() {
         return (
             <section className="converter">
-                <p className="converter-title">Convert a number into a list of words</p>
-                <form onSubmit={this.submitNumber} action="#">
-                    <label>Insert your number:</label>
-                    <input
-                        className=""
-                        type="number"
-                        onChange={event => this.updateNumber(event)}
-                        value={this.state.number}
-                    />
-                    <input type="submit" value="Submit"/>
-                </form>
-                {this.state.error.length > 0 ?
-                    <p className="error">
-                        {this.state.error}
-                    </p> : <p></p>}
+                <div>
+                    <p className="converter-title">Insert a number</p>
 
-                {this.state.wordList.map(word =>
-                    <div key={word}>
-                        <p>{word}</p>
-                    </div>
-                )}
-                <div className="converter-phone">
-                    <div className="converter-phone-container">
-                        <div className="converter-phone-container-screen">{this.state.number}</div>
-                        <div className="converter-phone-numpad">
-                            <button className="converter-phone-numpad-item">1
-                                <div className="letters">&nbsp;</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">2
-                                <div className="letters">abc</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">3
-                                <div className="letters">def</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">4
-                                <div className="letters">ghi</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">5
-                                <div className="letters">jkl</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">6
-                                <div className="letters">mno</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">7
-                                <div className="letters">pqrs</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">8
-                                <div className="letters">tuv</div>
-                            </button>
-                            <button className="converter-phone-numpad-item">9
-                                <div className="letters">wxyz</div>
-                            </button>
+                    <div className="converter-phone">
+                        <div className="converter-phone-container">
+                            <div className=""></div>
+                            <div className="converter-phone-container-screen">{this.state.number}</div>
+                            <div className="converter-phone-numpad">
+                                <button onClick={() => this.updateNumber(1)} className="converter-phone-numpad-item">1
+                                    <div className="letters">&nbsp;</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(2)} className="converter-phone-numpad-item">2
+                                    <div className="letters">abc</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(3)} className="converter-phone-numpad-item">3
+                                    <div className="letters">def</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(4)} className="converter-phone-numpad-item">4
+                                    <div className="letters">ghi</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(5)} className="converter-phone-numpad-item">5
+                                    <div className="letters">jkl</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(6)} className="converter-phone-numpad-item">6
+                                    <div className="letters">mno</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(7)} className="converter-phone-numpad-item">7
+                                    <div className="letters">pqrs</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(8)} className="converter-phone-numpad-item">8
+                                    <div className="letters">tuv</div>
+                                </button>
+                                <button onClick={() => this.updateNumber(9)} className="converter-phone-numpad-item">9
+                                    <div className="letters">wxyz</div>
+                                </button>
+                            </div>
+                            <div className="converter-phone-container-actions">
+                                <button onClick={this.cancelNumber}
+                                        className="converter-phone-container-actions-cancel">Cancel
+                                </button>
+                                <button onClick={this.submitNumber}
+                                        className="converter-phone-container-actions-submit">Submit
+                                </button>
+                            </div>
                         </div>
-                        <div className="converter-phone-container-actions">
-                            <button className="converter-phone-container-actions-cancel">Cancel</button>
-                            <button className="converter-phone-container-actions-submit">Submit</button>
-                        </div>
                     </div>
+                </div>
+                <div>
+                    <p className="converter-title">Convert into words</p>
+                    {this.state.error.length > 0 ?
+                        <p className="error">
+                            {this.state.error}
+                        </p> : <p></p>}
+
+                    {this.state.wordList.map(word =>
+                        <div key={word}>
+                            <p>{word}</p>
+                        </div>
+                    )}
                 </div>
             </section>
         );
